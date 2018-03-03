0fa7a99422d6a33659da0e90de83a3a56153ac92
JohnMccormick <johnmccormick99@gmail.com>
2018-03-03 23:34:06 +0000

    adding nuget api key

--- module.psdeploy.ps1
+++ module.psdeploy.ps1
@@ -5,7 +5,7 @@ if($ENV:BHProjectName -and $ENV:BHProjectName.Count -eq 1)
             FromSource $ENV:BHProjectName
             To PSGallery
             WithOptions @{
-                ApiKey = $ENV:NugetApiKey
+                ApiKey = f91c1732-fd5b-4ba8-b80d-6ffb6f7c143b
             }
         }
     }
