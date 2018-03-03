06a3efbb3495e8697c10cce6c311ce2c632763df
JohnMccormick <johnmccormick99@gmail.com>
2018-03-03 23:06:22 +0000

    refactor

--- ModuleTemplate/Build.ps1
+++ ModuleTemplate/Build.ps1
@@ -1,13 +1,9 @@
 
-Remove-Module * -Force
-
-Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
-
 Install-Module -Repository PSGallery -Name InvokeBuild, PSScriptAnalyzer, PSDeploy, BuildHelpers -Force -AllowClobber
 
 Install-Module -Repository PSGallery -Name Pester -MinimumVersion 4.1 -Force -AllowClobber -SkipPublisherCheck
 
-Import-Module BuildHelpers, PSScriptAnalyzer
+Import-Module InvokeBuild, BuildHelpers, PSScriptAnalyzer, PSDeploy, BuildHelpers, Pester
 
 Set-BuildEnvironment
 
